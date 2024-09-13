b0f6bcaa4bc934eb4c7809171bf3c633fc30c3f5
Luis Jose Sanchez <luisjoseprofe@gmail.com>
2024-09-13 17:37:02 +0200

    Correction in upload page

--- app/components/file-viewer.tsx
+++ app/components/file-viewer.tsx
@@ -62,7 +62,7 @@ const FileViewer = () => {
         }`}
       >
         {files.length === 0 ? (
-          <div className={styles.title}>Attach files to test file search</div>
+          <div className={styles.title}>Attach file with your story</div>
         ) : (
           files.map((file) => (
             <div key={file.file_id} className={styles.fileEntry}>
@@ -79,7 +79,7 @@ const FileViewer = () => {
       </div>
       <div className={styles.fileUploadContainer}>
         <label htmlFor="file-upload" className={styles.fileUploadBtn}>
-          Attach file with your story
+          Upload file
         </label>
         <input
           type="file"
